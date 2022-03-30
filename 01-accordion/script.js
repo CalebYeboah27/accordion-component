5dc06fa6b895a36caa2ecc62e9cad1c81ca5d438
Caleb Yeboah <44115413+CalebYeboah27@users.noreply.github.com>
2022-03-30 14:44:43 +0000

    Refactored code
    Refactored using short circuiting in-place of if-statement and for-each in-place of for-of.

--- 01-accordion/script.js
+++ 01-accordion/script.js
@@ -1,14 +1,29 @@
 const items = document.querySelectorAll(".item");
 const hiddenBox = document.querySelectorAll(".hidden-box");
 
-for (const [index, item] of items.entries()) {
+// UNUSED PARAMETERS kept for reference
+items.forEach((item, index, items) => {
   item.addEventListener("click", function (e) {
     const target = Number(e.target.id);
-
-    for (const [index, value] of hiddenBox.entries()) {
-      if (target === index + 1) {
-        value.classList.toggle("open");
-      }
-    }
+    // value represents a single hiddenbox
+    hiddenBox.forEach((value, index) => {
+      // short circuiting used in place of if statement
+      target === index + 1 && value.classList.toggle("open");
+    });
   });
-}
+});
+
+// ALTERNATIVELY
+
+// for (const [index, item] of items.entries()) {
+//   item.addEventListener("click", function (e) {
+//     const target = Number(e.target.id);
+
+//     for (const [index, value] of hiddenBox.entries()) {
+//       if (target === index + 1) {
+//         // value represents a single hiddenbox
+//         value.classList.toggle("open");
+//       }
+//     }
+//   });
+// }
